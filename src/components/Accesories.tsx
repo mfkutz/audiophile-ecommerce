78649d46d19814a564777bc923252e8c19d3937f
Martin <mfkutz@gmail.com>
2024-12-16 20:46:37 -0300

    conditional in image large

--- src/components/Accesories.tsx
+++ src/components/Accesories.tsx
@@ -3,9 +3,9 @@ import Card from "./accesories/Card"
 export default function Accesories() {
     return (
         <div className="mx-auto max-w-screen-mk flex flex-col acc:flex-row justify-between my-20 sps:my-32 acc:my-[200px]  gap-20 sps:gap-28 acc:gap-6">
-            <Card />
-            <Card />
-            <Card />
+            <Card element="headphones" image="./image-removebg-preview(41).png" move="36%" />
+            <Card element="speakers" image="./image-removebg-preview(38).png" move="36%" />
+            <Card element="earphones" image="./image-removebg-preview(42).png" move="30%" isLarge={true} />
         </div>
     )
 }
--- src/components/accesories/Card.tsx
+++ src/components/accesories/Card.tsx
@@ -1,20 +1,26 @@
+type CardProps = {
+    element: string
+    image: string
+    move?: string
+    isLarge?: boolean
+}
 
-export default function Card() {
+export default function Card({ element, image, move, isLarge }: CardProps) {
     return (
         <div
             className="flex flex-col border bg-white-ec rounded-md w-full text-center h-[160px] sps:h-[200px] justify-end py-7 items-center relative"
         >
             <div className=" bg-black w-[140px] h-[20px] rounded-full blur-xl absolute top-[35%]"></div>
             <img
-                src="./image-removebg-preview(41).png"
-                alt=""
-                className="absolute top-[-30%] sps:top-[-40%] w-[79.95px] sps:w-[122.95px]"
+                src={image}
+                alt="icon"
+                className={`absolute top-[-30%] sps:top-[-${move}] ${isLarge ? "w-[100px]" : "w-[88px]"}  sps:w-[122.95px]`}
             />
-            <p className="uppercase font-bold text-[15px] sps:text-[18px] mb-[5px] tracking-wider ">headphones</p>
+            <p className="uppercase font-bold text-[15px] sps:text-[18px] mb-[5px] tracking-wider ">{element}</p>
             <button
                 className=" flex items-center gap-2 uppercase text-[13px] tracking-[1.5px] font-semibold text-gray-700 hover:text-more-ec"
             >shop
-                <img src="./icon-arrow-right.svg" alt="" />
+                <img src="icon-arrow-right.svg" alt="arrow" />
             </button>
         </div>
     )
